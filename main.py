1acbf81e2bc0b0c49a62311bafb43edd8b87813a
user <user@users-MacBook-Pro.local>
2026-07-26 15:56:56 +0200

    Subbtraction of two numbers

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ num2 = int(input("Enter num2: "))
 if sign == '+':
     pass
 elif sign == '-':
-    pass
+    def subtraction(a, b):
+        """
+        Subtracts two numbers.
+
+        Parameters:
+        a (float): The first number.
+        b (float): The second number.
+
+        Returns:
+        float: The result of subtracting b from a.
+        """
+        return a - b
 elif sign == '*':
     pass
 elif sign == '/':
