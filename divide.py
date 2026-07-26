65ad19a20b22b902335a7c3f4a7af3f9dc1edcd2
Terrence <user@MacBookPro.home>
2026-07-26 14:52:37 +0200

    added division module

---
+++ divide.py
@@ -0,0 +1,28 @@
+def divide(num1, num2):
+    """
+    This funtion gets 2 numbers and returns the quatient
+
+    args
+        num1 :int
+        num2 :int
+
+    return
+        quatient: float
+    """
+    if num2 == 0:
+        return "not divisible by zero"
+    return num1 / num2
+
+
+# def get_name(name) -> str:
+#     """
+#     this function gets a name and return
+
+
+#     args
+#      name : str
+
+#     return
+#      name str
+
+#     """
