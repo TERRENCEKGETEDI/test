e8a924df30bea5a1c472564dc92fcb9e6d2cbd01
Bongiwe Magagule <believebongiwe@gmail.com>
2026-07-26 14:52:31 +0200

    Added a multiplication function

---
+++ multiplication.py
@@ -0,0 +1,15 @@
+
+def multiplication(num1,num2):
+    """
+    this funtions return product of two numbers
+    
+    args
+    num1:int
+    num2:int
+    
+    return 
+    answer: int
+    """
+    
+    return num1*num2
+
