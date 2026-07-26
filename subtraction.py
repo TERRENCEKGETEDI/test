0612646c728b5fd60210dd0c6824eb2c9804277b
user <user@users-MacBook-Pro.local>
2026-07-26 15:32:29 +0200

    Subbtraction of two numbers

---
+++ subtraction.py
@@ -0,0 +1,13 @@
+
+def subtraction(a, b):
+    """
+    Subtracts two numbers.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The result of subtracting b from a.
+    """
+    return a - b
